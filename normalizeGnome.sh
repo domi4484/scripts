bd2218a8533323ad6c654c13c267b5a49787c7c3
Damiano Lombardi <dlombardi@hamilton.ch>
2016-11-15 10:34:12 +0100

    update

--- normalizeGnome.sh
+++ normalizeGnome.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
+
+gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+
+gsettings set org.gnome.shell.overrides attach-modal-dialogs false
+
+
+
+
+# export GTK_IM_MODULE="gtk-im-context-simple"
+# export QT_IM_MODULE="simple"
+#
+# in /etc/profile.d
 
