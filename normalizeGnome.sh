ded6cd357d7d0be6f7b03aa82c3e578c5ba2b0c0
Damiano Lombardi <dlombardi@hamilton.ch>
2016-11-15 15:11:53 +0100

    updated

--- normalizeGnome.sh
+++ normalizeGnome.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
+echo "------------------------------"
+echo "Setup Nemo instead of nautilus"
+echo "------------------------------"
+
+#TODO apt repo for nemo w8d...
+sudo apt-get install nemo
+
+xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
+gsettings set org.gnome.desktop.background show-desktop-icons false
+gsettings set org.nemo.desktop show-desktop-icons true
+xdg-mime query default inode/directory
+
+
+echo "--------------------------------"
+echo "Move and resize windows with Alt"
+echo "--------------------------------"
+
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier ’<Alt>’
+
+echo "-------------------------------"
+echo "Show icons on buttons and menus"
+echo "-------------------------------"
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gtk/ButtonImages': <1>, 'Gtk/MenuImages': <1>}"
+
 
 gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
 
