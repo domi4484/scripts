03580492f4c7dd5dce6ac2c5385a6b6c2a62e468
Gluekler Stefan <sgluekler@hamilton.ch>
2017-06-20 08:52:14 +0200

    ...

--- normalizeOpensuseXFCE.sh
+++ normalizeOpensuseXFCE.sh
@@ -4,3 +4,8 @@ echo "-------------------------------"
 echo "Show icons on buttons and menus"
 echo "-------------------------------"
 gconftool-2 –type bool –set /desktop/gnome/interface/menus_have_icons true
+
+echo "------------------"
+echo "Remove system bell"
+echo "------------------"
+echo "set bell-style none" >> ~/.inputrc
