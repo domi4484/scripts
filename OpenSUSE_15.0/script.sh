5d9f668be7d367ad7f0fcb3b6f62617f0a38ab8f
Damiano Lombardi <lombardi@customcut.ch>
2018-06-12 19:45:26 +0200

    ...

--- OpenSUSE_15.0/script.sh
+++ OpenSUSE_15.0/script.sh
@@ -1,5 +1,5 @@
 
 
-
+# https://forums.opensuse.org/showthread.php/529533-Tumbleweed-upgrade-from20180202-to-20180203-breaks-FreeCAD?p=2861858#post2861858
 # LIBGL_DRI3_DISABLE=true" to /etc/environment 
 
